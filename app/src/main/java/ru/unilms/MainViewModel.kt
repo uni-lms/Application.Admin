55a0382639fab433f40988f43f3b037285b99bd4
Daniil <dadyarri@gmail.com>
2023-09-07 17:09:16 +0300

    add form to viewmodel

--- app/src/main/java/ru/unilms/MainViewModel.kt
+++ app/src/main/java/ru/unilms/MainViewModel.kt
@@ -1,9 +1,17 @@
 package ru.unilms
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ru.unilms.di.ResourcesProvider
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(resourcesProvider: ResourcesProvider) : ViewModel()
+class MainViewModel @Inject constructor(resourcesProvider: ResourcesProvider) : ViewModel() {
+    var form = SignUpForm(resourcesProvider)
+
+    fun validate() {
+        form.validate(true)
+        Log.d("MainViewModel", "Validate (form is valid: ${form.isValid})")
+    }
+}
