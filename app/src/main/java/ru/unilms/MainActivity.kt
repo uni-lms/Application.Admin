524f0de6b7ba9d8c4130f5908e286a0ce41dc20e
Daniil <dadyarri@gmail.com>
2023-09-07 18:05:42 +0300

    change translation of components method

--- app/src/main/java/ru/unilms/MainActivity.kt
+++ app/src/main/java/ru/unilms/MainActivity.kt
@@ -3,17 +3,16 @@ package ru.unilms
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.os.LocaleListCompat
 import dagger.hilt.android.AndroidEntryPoint
 import ru.unilms.app.UniApp
 import ru.unilms.ui.theme.UNITheme
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags("ru"))
+        Locale.setDefault(Locale("ru", "RU"))
         setContent {
             UNITheme {
                 UniApp()
