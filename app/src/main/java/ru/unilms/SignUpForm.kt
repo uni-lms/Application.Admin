1606fb09e8748e38643ad2ea7fcab65454e4d784
Daniil <dadyarri@gmail.com>
2023-09-07 17:09:45 +0300

    add fields to signup form

--- app/src/main/java/ru/unilms/SignUpForm.kt
+++ app/src/main/java/ru/unilms/SignUpForm.kt
@@ -1,10 +1,52 @@
 package ru.unilms
 
-import android.content.res.loader.ResourcesProvider
+import ru.unilms.di.ResourcesProvider
+import androidx.compose.runtime.mutableStateOf
+import ch.benlu.composeform.FieldState
 import ch.benlu.composeform.Form
+import ch.benlu.composeform.FormField
+import ch.benlu.composeform.validators.EmailValidator
+import ch.benlu.composeform.validators.NotEmptyValidator
+import java.util.Date
 
 class SignUpForm(resourcesProvider: ResourcesProvider): Form() {
     override fun self(): Form {
         return this
     }
+
+    @FormField
+    val firstName = FieldState(
+        state = mutableStateOf<String?>(null),
+        validators = mutableListOf(NotEmptyValidator())
+    )
+
+    @FormField
+    val lastName = FieldState(
+        state = mutableStateOf<String?>(null),
+        validators = mutableListOf(NotEmptyValidator())
+    )
+
+    @FormField
+    val patronymic = FieldState(
+        state = mutableStateOf<String?>(null),
+        validators = mutableListOf(NotEmptyValidator())
+    )
+
+    @FormField
+    val email = FieldState(
+        state = mutableStateOf<String?>(null),
+        validators = mutableListOf(
+            EmailValidator()
+        )
+    )
+
+    @FormField
+    val dateOfBirth = FieldState(
+        state = mutableStateOf<Date?>(null),
+        validators = mutableListOf(
+            NotEmptyValidator()
+        )
+    )
+
+
 }
