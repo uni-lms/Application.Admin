505826801972530d0e1ec68005cad284e9e0f807
Daniil <dadyarri@gmail.com>
2023-09-12 21:15:05 +0300

    add some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material-android:1.5.1")
     implementation("androidx.compose.material3:material3:1.1.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.compose.material:material-icons-extended-android:1.5.0")
