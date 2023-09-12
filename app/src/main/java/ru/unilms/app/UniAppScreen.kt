e4bb71d8fab842bfdde7252f9a8e2ab7b65a4e11
Daniil <dadyarri@gmail.com>
2023-09-12 19:54:38 +0300

    add extra parameters to screens definition

--- app/src/main/java/ru/unilms/app/UniAppScreen.kt
+++ app/src/main/java/ru/unilms/app/UniAppScreen.kt
@@ -2,11 +2,15 @@ package ru.unilms.app
 
 import ru.unilms.R
 
-enum class UniAppScreen(val title: Int = R.string.app_name) {
-    SelectApiUri(R.string.screen_server_select),
+enum class UniAppScreen(
+    val title: Int = R.string.app_name,
+    val canGoBack: Boolean = true,
+    val showBottomAppBar: Boolean = false
+) {
+    SelectApiUri(R.string.screen_server_select, false),
     LoginOrRegister,
     Login(R.string.login),
     SignUp(R.string.register),
-    Feed(R.string.feed),
-    Calendar(R.string.calendar),
+    Feed(R.string.feed, false, true),
+    Calendar(R.string.calendar, false, true),
 }
