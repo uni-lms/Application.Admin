60769929398f39b07240c1e503e77c4d769288ec
Daniil <dadyarri@gmail.com>
2023-09-12 21:14:43 +0300

    update style of text

--- app/src/main/java/ru/unilms/components/typography/CenteredRegularHeadline.kt
+++ app/src/main/java/ru/unilms/components/typography/CenteredRegularHeadline.kt
@@ -1,10 +1,21 @@
 package ru.unilms.components.typography
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 
 @Composable
-fun CenteredRegularHeadline(text: String, modifier: Modifier = Modifier) {
-    RegularHeadline(text = text, textAlign = TextAlign.Center, modifier = modifier)
+fun CenteredRegularHeadline(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onBackground
+) {
+    RegularHeadline(
+        text = text,
+        textAlign = TextAlign.Center,
+        modifier = modifier,
+        color = color
+    )
 }
--- app/src/main/java/ru/unilms/components/typography/RegularHeadline.kt
+++ app/src/main/java/ru/unilms/components/typography/RegularHeadline.kt
@@ -1,27 +1,26 @@
 package ru.unilms.components.typography
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun RegularHeadline(
     text: String,
     modifier: Modifier = Modifier,
-    textAlign: TextAlign = TextAlign.Left
+    textAlign: TextAlign = TextAlign.Left,
+    color: Color
 ) {
     Text(
         text = text,
         modifier = modifier
-            .fillMaxWidth()
-            .heightIn(min = 80.dp),
+            .fillMaxWidth(),
         style = MaterialTheme.typography.headlineMedium,
-        color = MaterialTheme.colorScheme.onBackground,
+        color = color,
         textAlign = textAlign
     )
 }
