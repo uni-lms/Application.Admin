578472f23a19eb0fdcf2ee9c00aa75c84d7bf471
Daniil <dadyarri@gmail.com>
2023-09-16 21:45:13 +0300

    update some styles

--- app/src/main/java/ru/unilms/components/courses/CourseCard.kt
+++ app/src/main/java/ru/unilms/components/courses/CourseCard.kt
@@ -1,6 +1,7 @@
 package ru.unilms.components.courses
 
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
@@ -13,13 +14,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.unilms.domain.model.courses.Course
 import java.util.UUID
 
 @Composable
-fun CourseCard(course: Course) {
+fun CourseCard(course: Course, goToCourseScreen: () -> Unit) {
 
     var progress by remember {
         mutableFloatStateOf(0f)
@@ -35,12 +37,17 @@ fun CourseCard(course: Course) {
         progress = course.progress / 100
     }
 
-    Surface(color = MaterialTheme.colorScheme.tertiaryContainer, shadowElevation = 5.dp) {
-        ListItem(headlineContent = {
-            Text(
-                text = course.name,
-            )
-        },
+    Surface(
+        color = MaterialTheme.colorScheme.tertiaryContainer,
+        shadowElevation = 5.dp,
+        modifier = Modifier.clickable(onClick = goToCourseScreen)
+    ) {
+        ListItem(
+            headlineContent = {
+                Text(
+                    text = course.name,
+                )
+            },
             trailingContent = {
                 CircularProgressIndicator(
                     progress = progressAnimate,
@@ -65,5 +72,5 @@ fun CourseCardPreview() {
             3,
             listOf("Вершинин В. В.")
         )
-    )
+    ) {}
 }
