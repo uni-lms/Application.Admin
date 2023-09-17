ef70b6c9db69c8dcbf3fb80bfa499e88b5ca0895
Daniil <dadyarri@gmail.com>
2023-09-17 19:47:11 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,16 +61,16 @@ dependencies {
     val retrofitVersion = "2.9.0"
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.1.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("androidx.compose.material:material-icons-extended-android:1.5.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.compose.material:material-icons-extended-android:1.5.1")
 
     // Forms
     implementation("com.github.benjamin-luescher:compose-form:0.2.3")
