e2e8ed70fa345793532706bc73bad54c09ef4ac5
Daniil <dadyarri@gmail.com>
2023-09-17 20:21:38 +0300

    BREAKING CHANGE: Change min required Android version to 11 (R)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.unilms"
-        minSdk = 27
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
