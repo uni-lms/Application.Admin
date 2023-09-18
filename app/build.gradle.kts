4aefe4936c619ac50f64db30284f2b173825e6e9
Daniil <dadyarri@gmail.com>
2023-09-19 01:29:03 +0300

    refactor build script; add git info to debug version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,13 @@
+val majorVersion = 1
+val minorVersion = 0
+val patchVersion = 0
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
     id("com.google.dagger.hilt.android")
+    id("com.sarhanm.versioner") version "4.1.9"
     kotlin("kapt")
 }
 
@@ -14,13 +19,11 @@ android {
         applicationId = "ru.unilms"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = majorVersion * 10000 + minorVersion * 100 + patchVersion
+        versionName = "$majorVersion.$minorVersion.$patchVersion"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        vectorDrawables.useSupportLibrary = true
     }
 
     buildTypes {
@@ -33,7 +36,7 @@ android {
         }
         getByName("debug") {
             applicationIdSuffix = ".debug"
-            versionNameSuffix = "-debug"
+            versionNameSuffix = "-debug (${gitdata.branch}@${gitdata.commit})"
         }
     }
     compileOptions {
@@ -59,26 +62,30 @@ android {
 dependencies {
 
     val retrofitVersion = "2.9.0"
+    val hiltVersion = "2.44"
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
+
+
+    // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.1.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.compose.material:material-icons-extended-android:1.5.1")
 
     // Forms
     implementation("com.github.benjamin-luescher:compose-form:0.2.3")
 
     // DI (Hilt)
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("com.google.dagger:hilt-android:${hiltVersion}")
+    kapt("com.google.dagger:hilt-android-compiler:${hiltVersion}")
 
     // Retrofit (http client)
     implementation("com.squareup.retrofit2:retrofit:${retrofitVersion}")
@@ -92,6 +99,7 @@ dependencies {
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // Debug stuff
     debugImplementation("androidx.compose.ui:ui-tooling")
