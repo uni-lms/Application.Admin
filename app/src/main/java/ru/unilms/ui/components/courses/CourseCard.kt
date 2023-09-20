a22ff516a2322a7d061ea01fbe081809fff7f4a3
Daniil <dadyarri@gmail.com>
2023-09-20 18:28:46 +0300

    move semester number label to string resources

--- app/src/main/java/ru/unilms/ui/components/courses/CourseCard.kt
+++ app/src/main/java/ru/unilms/ui/components/courses/CourseCard.kt
@@ -9,8 +9,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ru.unilms.R
 import ru.unilms.domain.model.courses.Course
 import java.util.UUID
 
@@ -41,7 +43,14 @@ fun CourseCard(course: Course, goToCourseScreen: () -> Unit) {
             )
         },
         supportingContent = { Text(text = course.tutors.joinToString()) },
-        overlineContent = { Text(text = "${course.semester} семестр") }
+        overlineContent = {
+            Text(
+                text = stringResource(
+                    id = R.string.label_semester,
+                    course.semester
+                )
+            )
+        }
     )
 }
 
