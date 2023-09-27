7aab4109857ae906bc6f014303644a281a9a5fc8
Daniil <dadyarri@gmail.com>
2023-09-27 22:03:59 +0300

    fix scroll on signup screen

--- app/src/main/java/ru/unilms/ui/screens/SignUpScreen.kt
+++ app/src/main/java/ru/unilms/ui/screens/SignUpScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -40,6 +42,7 @@ fun SignUpScreen(goToFeedScreen: () -> Unit) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
             ImagePickerField(
                 modifier = Modifier.align(Alignment.CenterHorizontally),
