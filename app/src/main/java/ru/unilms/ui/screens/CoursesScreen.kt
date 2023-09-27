4db86291affb9a064bc012bb93344ae9279d0929
Daniil <dadyarri@gmail.com>
2023-09-27 22:13:30 +0300

    add remember to data

--- app/src/main/java/ru/unilms/ui/screens/CoursesScreen.kt
+++ app/src/main/java/ru/unilms/ui/screens/CoursesScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import ru.unilms.ui.components.courses.CourseCard
@@ -13,9 +16,10 @@ import java.util.UUID
 @Composable
 fun CoursesScreen(goToCourseScreen: (UUID) -> Unit) {
     val viewModel = hiltViewModel<CoursesScreenViewModel>()
+    val courses by remember { mutableStateOf(viewModel.courses) }
 
     LazyColumn(modifier = Modifier.fillMaxHeight()) {
-        items(items = viewModel.courses, itemContent = { item ->
+        items(items = courses, itemContent = { item ->
             CourseCard(course = item) {
                 goToCourseScreen(item.id)
             }
