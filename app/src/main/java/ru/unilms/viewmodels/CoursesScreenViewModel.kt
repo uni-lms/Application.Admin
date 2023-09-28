4b912abe33834ba3125b5d088fd7e75a35634d02
Daniil <dadyarri@gmail.com>
2023-09-28 10:08:13 +0300

    add different courses by its type

--- app/src/main/java/ru/unilms/viewmodels/CoursesScreenViewModel.kt
+++ app/src/main/java/ru/unilms/viewmodels/CoursesScreenViewModel.kt
@@ -3,6 +3,7 @@ package ru.unilms.viewmodels
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ru.unilms.domain.model.courses.Course
+import ru.unilms.utils.enums.CourseType
 import java.util.UUID
 import javax.inject.Inject
 
@@ -11,47 +12,62 @@ class CoursesScreenViewModel @Inject constructor() : ViewModel() {
 
     var isLoading = false
 
-    fun loadCourses(): List<Course> {
-        isLoading = true
-        val courses = listOf(
-            Course(
-                UUID.randomUUID(),
-                "Технологии программирования",
-                80f,
-                3,
-                listOf("Вершинин В. В.", "Данилов В. В.")
-            ),
-            Course(
-                UUID.randomUUID(),
-                "Алгоритмы и структуры данных",
-                20f,
-                2,
-                listOf("Шамышева О. Н.")
-            ),
-            Course(
-                UUID.randomUUID(),
-                "Информационные технологии в образовании",
-                40f,
-                7,
-                listOf("Озерова М. И.")
-            ),
-            Course(
-                UUID.randomUUID(),
-                "Графические информационные технологии",
-                50f,
-                5,
-                listOf("Монахова Г. Е.")
-            ),
-            Course(UUID.randomUUID(), "Графический и веб-дизайн", 10f, 7, listOf("Шамышев Х. Б.")),
-            Course(
-                UUID.randomUUID(),
-                "Качество программно-информационных систем",
-                5f,
-                7,
-                listOf("Ланская М. К.")
-            )
+    private val archivedCourses = listOf(
+        Course(
+            UUID.randomUUID(),
+            "Технологии программирования",
+            80f,
+            3,
+            listOf("Вершинин В. В.", "Данилов В. В.")
+        ),
+        Course(
+            UUID.randomUUID(),
+            "Алгоритмы и структуры данных",
+            20f,
+            2,
+            listOf("Шамышева О. Н.")
         )
+    )
+
+    private val currentCourses = listOf(
+        Course(
+            UUID.randomUUID(),
+            "Графические информационные технологии",
+            50f,
+            5,
+            listOf("Монахова Г. Е.")
+        )
+    )
+
+    private val futureCourses = listOf(
+        Course(
+            UUID.randomUUID(),
+            "Информационные технологии в образовании",
+            40f,
+            7,
+            listOf("Озерова М. И.")
+        ),
+        Course(UUID.randomUUID(), "Графический и веб-дизайн", 10f, 7, listOf("Шамышев Х. Б.")),
+        Course(
+            UUID.randomUUID(),
+            "Качество программно-информационных систем",
+            5f,
+            7,
+            listOf("Ланская М. К.")
+        )
+    )
+
+    fun loadCourses(coursesType: CourseType = CourseType.Current): List<Course> {
+        isLoading = true
+
+        val courses = when (coursesType) {
+            CourseType.Archived -> archivedCourses
+            CourseType.Current -> currentCourses
+            CourseType.Future -> futureCourses
+        }
+
         isLoading = false
+
         return courses
     }
 
