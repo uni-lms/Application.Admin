8b04027680158c2d7dcb983cc99fc7234d81afb7
Daniil <dadyarri@gmail.com>
2023-09-29 20:23:36 +0300

    add fake login screen

--- app/src/main/java/ru/unilms/ui/screens/LoginScreen.kt
+++ app/src/main/java/ru/unilms/ui/screens/LoginScreen.kt
@@ -1,7 +1,69 @@
 package ru.unilms.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import ru.unilms.R
+import ru.unilms.ui.components.form.M3TextField
+import ru.unilms.viewmodels.LoginViewModel
 
 @Composable
 fun LoginScreen(goToFeedScreen: () -> Unit) {
+    val viewModel = hiltViewModel<LoginViewModel>()
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(28.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            M3TextField(
+                label = stringResource(id = R.string.field_email),
+                form = viewModel.form,
+                fieldState = viewModel.form.email,
+                keyboardType = KeyboardType.Email,
+                keyboardCapitalization = KeyboardCapitalization.None,
+                modifier = Modifier.background(MaterialTheme.colorScheme.background)
+            ).Field()
+            M3TextField(
+                label = stringResource(id = R.string.field_password),
+                form = viewModel.form,
+                fieldState = viewModel.form.password,
+                keyboardType = KeyboardType.Password,
+                keyboardCapitalization = KeyboardCapitalization.None,
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.background(MaterialTheme.colorScheme.background)
+            ).Field()
+
+            Column(horizontalAlignment = Alignment.End, modifier = Modifier.fillMaxWidth()) {
+                Button(onClick = { viewModel.submit { goToFeedScreen() } }) {
+                    Text(text = stringResource(R.string.button_login))
+                }
+            }
+        }
+
+
+    }
 }
---
+++ app/src/main/java/ru/unilms/viewmodels/LoginViewModel.kt
@@ -0,0 +1,43 @@
+package ru.unilms.viewmodels
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
+import ru.unilms.data.DataStore
+import ru.unilms.domain.model.auth.LoginRequest
+import ru.unilms.ui.forms.LoginForm
+import javax.inject.Inject
+
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(@ApplicationContext context: Context) : ViewModel() {
+
+    private val store = DataStore(context)
+
+    private fun validate() {
+        form.validate(true)
+        Log.d("MainViewModel", "Validate (form is valid: ${form.isValid})")
+    }
+
+    fun submit(goToFeedScreen: () -> Unit) {
+        validate()
+        if (form.isValid) {
+            val signupRequest = LoginRequest(
+                email = form.email.state.value!!,
+                password = form.password.state.value!!,
+            )
+            viewModelScope.launch {
+                // TODO: put here value from api response
+                store.updateToken("some-not-empty-value")
+            }
+            goToFeedScreen()
+        }
+    }
+
+    var form = LoginForm()
+
+}
