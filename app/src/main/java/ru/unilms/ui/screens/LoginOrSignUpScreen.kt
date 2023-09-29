a17a19a96e0238bc9a34758e6301fa6e175c35b4
Daniil <dadyarri@gmail.com>
2023-09-29 20:09:45 +0300

    add navigation to login screen

--- app/src/main/java/ru/unilms/ui/screens/LoginOrSignUpScreen.kt
+++ app/src/main/java/ru/unilms/ui/screens/LoginOrSignUpScreen.kt
@@ -6,29 +6,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
-import kotlinx.coroutines.launch
 import ru.unilms.R
-import ru.unilms.viewmodels.UniAppViewModel
 
 @Composable
 fun LoginOrSignUpScreen(goToLoginScreen: () -> Unit, goToSignUpScreen: () -> Unit) {
-
-    val viewModel = hiltViewModel<UniAppViewModel>()
-    val scope = rememberCoroutineScope()
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        TextButton(onClick = {
-            scope.launch {
-                viewModel.store.updateToken("not-empty-value")
-            }
-        }) {
+        TextButton(onClick = goToLoginScreen) {
             Text(
                 text = stringResource(R.string.screen_login),
                 style = MaterialTheme.typography.headlineSmall
