5ea72cafa4dfac7081808009051ff1bcfd85f061
Daniil <dadyarri@gmail.com>
2023-10-02 12:41:58 +0300

    fix updating month events info

--- app/src/main/java/ru/unilms/ui/screens/CalendarScreen.kt
+++ app/src/main/java/ru/unilms/ui/screens/CalendarScreen.kt
@@ -18,22 +18,27 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.kizitonwose.calendar.compose.CalendarLayoutInfo
+import com.kizitonwose.calendar.compose.CalendarState
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
 import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.CalendarMonth
 import com.kizitonwose.calendar.core.daysOfWeek
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.nextMonth
+import com.kizitonwose.calendar.core.previousMonth
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 import ru.unilms.data.AppBarState
 import ru.unilms.ui.components.calendar.Day
 import ru.unilms.ui.components.calendar.DaysOfWeek
 import ru.unilms.ui.components.calendar.MonthName
 import ru.unilms.viewmodels.CalendarViewModel
-import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
@@ -43,7 +48,7 @@ fun CalendarScreen(onComposing: (AppBarState) -> Unit) {
     val viewModel = hiltViewModel<CalendarViewModel>()
     val firstDayOfWeek = remember { firstDayOfWeekFromLocale() }
     val daysOfWeek = daysOfWeek()
-    var currentMonth = remember { YearMonth.now() }
+    val currentMonth = remember { YearMonth.now() }
     val startMonth = remember { currentMonth.minusMonths(100) }
     val endMonth = remember { currentMonth.plusMonths(100) }
     val state = rememberCalendarState(
@@ -56,7 +61,14 @@ fun CalendarScreen(onComposing: (AppBarState) -> Unit) {
     var data by remember { mutableStateOf(viewModel.requestDataForMonth(currentMonth)) }
     var isModalOpened by remember { mutableStateOf(false) }
     var selectedDay: CalendarDay? by remember {
-        mutableStateOf(CalendarDay(LocalDate.now(), DayPosition.MonthDate))
+        mutableStateOf(null)
+    }
+    val visibleMonth = rememberFirstCompletelyVisibleMonth(state)
+    LaunchedEffect(visibleMonth) {
+        // Clear selection if we scroll to a new month.
+        selectedDay = null
+        isModalOpened = false
+        data = viewModel.requestDataForMonth(visibleMonth.yearMonth)
     }
 
     LaunchedEffect(key1 = true) {
@@ -65,11 +77,8 @@ fun CalendarScreen(onComposing: (AppBarState) -> Unit) {
                 actions = {
                     IconButton(
                         onClick = {
-                            val month = currentMonth.minusMonths(1)
-                            currentMonth = month
-                            data = viewModel.requestDataForMonth(currentMonth)
                             scope.launch {
-                                state.animateScrollToMonth(month)
+                                state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.previousMonth)
                             }
                         }
                     ) {
@@ -77,11 +86,8 @@ fun CalendarScreen(onComposing: (AppBarState) -> Unit) {
                     }
                     IconButton(
                         onClick = {
-                            val month = currentMonth.plusMonths(1)
-                            currentMonth = month
-                            data = viewModel.requestDataForMonth(currentMonth)
                             scope.launch {
-                                state.animateScrollToMonth(month)
+                                state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.nextMonth)
                             }
                         }) {
                         Icon(Icons.Outlined.ChevronRight, null)
@@ -137,3 +143,35 @@ fun CalendarScreen(onComposing: (AppBarState) -> Unit) {
     }
 
 }
+
+@Composable
+fun rememberFirstCompletelyVisibleMonth(state: CalendarState): CalendarMonth {
+    val visibleMonth = remember(state) { mutableStateOf(state.firstVisibleMonth) }
+    // Only take non-null values as null will be produced when the
+    // list is mid-scroll as no index will be completely visible.
+    LaunchedEffect(state) {
+        snapshotFlow { state.layoutInfo.completelyVisibleMonths.firstOrNull() }
+            .filterNotNull()
+            .collect { month -> visibleMonth.value = month }
+    }
+    return visibleMonth.value
+}
+
+private val CalendarLayoutInfo.completelyVisibleMonths: List<CalendarMonth>
+    get() {
+        val visibleItemsInfo = this.visibleMonthsInfo.toMutableList()
+        return if (visibleItemsInfo.isEmpty()) {
+            emptyList()
+        } else {
+            val lastItem = visibleItemsInfo.last()
+            val viewportSize = this.viewportEndOffset + this.viewportStartOffset
+            if (lastItem.offset + lastItem.size > viewportSize) {
+                visibleItemsInfo.removeLast()
+            }
+            val firstItem = visibleItemsInfo.firstOrNull()
+            if (firstItem != null && firstItem.offset < this.viewportStartOffset) {
+                visibleItemsInfo.removeFirst()
+            }
+            visibleItemsInfo.map { it.month }
+        }
+    }
