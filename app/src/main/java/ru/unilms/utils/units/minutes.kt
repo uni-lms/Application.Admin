e76d65702585cb68490f080f17672a2792374f30
Daniil <dadyarri@gmail.com>
2023-10-24 23:07:10 +0300

    update visual of minutes converter

--- app/src/main/java/ru/unilms/utils/units/minutes.kt
+++ app/src/main/java/ru/unilms/utils/units/minutes.kt
@@ -1,14 +1,15 @@
 package ru.unilms.utils.units
 
 import java.time.Duration
-import java.time.LocalTime
 
 inline val Int.minutes: String get() = Minutes(value = this).toString()
 
 data class Minutes(val value: Int) {
     override fun toString(): String {
-        return LocalTime.MIN.plus(
-            Duration.ofMinutes(value.toLong())
-        ).toString()
+        val duration = Duration.ofMinutes(value.toLong())
+        return when (duration.toHours()) {
+            0L -> "${duration.toMinutes()} минут"
+            else -> "${duration.toHours()} часов ${duration.toMinutes()} минут"
+        }
     }
 }
