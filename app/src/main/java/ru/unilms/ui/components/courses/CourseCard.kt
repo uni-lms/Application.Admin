c43efc9e291dbb1f1488bce88fcac32136c577c0
Daniil <dadyarri@gmail.com>
2023-10-27 14:50:27 +0300

    remove preview

--- app/src/main/java/ru/unilms/ui/components/courses/CourseCard.kt
+++ app/src/main/java/ru/unilms/ui/components/courses/CourseCard.kt
@@ -10,11 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.unilms.R
 import ru.unilms.domain.model.courses.Course
-import java.util.UUID
 
 @Composable
 fun CourseCard(course: Course, goToCourseScreen: () -> Unit) {
@@ -52,18 +50,4 @@ fun CourseCard(course: Course, goToCourseScreen: () -> Unit) {
             )
         }
     )
-}
-
-@Composable
-@Preview
-fun CourseCardPreview() {
-    CourseCard(
-        course = Course(
-            UUID.randomUUID(),
-            "ТП",
-            35f,
-            3,
-            listOf("Вершинин В. В.")
-        )
-    ) {}
 }
