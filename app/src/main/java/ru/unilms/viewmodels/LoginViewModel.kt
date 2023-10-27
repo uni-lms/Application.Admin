054b25fd1efca4636232b2bfb1e9f8fa7fee4d4d
Daniil <dadyarri@gmail.com>
2023-10-27 12:12:13 +0300

    add calling real login endpoint (i don't like duplication, that will be in error processing, but that's all, that we can have)

--- app/src/main/java/ru/unilms/viewmodels/LoginViewModel.kt
+++ app/src/main/java/ru/unilms/viewmodels/LoginViewModel.kt
@@ -2,6 +2,7 @@ package ru.unilms.viewmodels
 
 import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,14 +10,28 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
 import ru.unilms.data.DataStore
 import ru.unilms.domain.model.auth.LoginRequest
+import ru.unilms.network.services.AuthService
+import ru.unilms.network.services.AuthServiceImpl
+import ru.unilms.network.services.Response
 import ru.unilms.ui.forms.LoginForm
 import javax.inject.Inject
 
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(@ApplicationContext context: Context) : ViewModel() {
+class LoginViewModel @Inject constructor(@ApplicationContext private val context: Context) :
+    ViewModel() {
 
-    private val store = DataStore(context)
+    private var store: DataStore? = null
+    private var baseUrl = ""
+
+    init {
+        store = DataStore(context)
+        viewModelScope.launch {
+            store!!.apiUri.collect {
+                baseUrl = it
+            }
+        }
+    }
 
     private fun validate() {
         form.validate(true)
@@ -24,17 +39,49 @@ class LoginViewModel @Inject constructor(@ApplicationContext context: Context) :
     }
 
     fun submit(goToFeedScreen: () -> Unit) {
-//        validate()
-        if (form.isValid || true) {
-            val signupRequest = LoginRequest(
+        validate()
+        if (form.isValid) {
+            val loginRequest = LoginRequest(
                 email = form.email.state.value ?: "",
                 password = form.password.state.value ?: "",
             )
             viewModelScope.launch {
-                // TODO: put here value from api response
-                store.updateToken("some-not-empty-value")
+                AuthService.client.use { client ->
+                    val service = AuthServiceImpl(client, baseUrl)
+
+                    when (val res = service.login(loginRequest)) {
+                        is Response.Success -> {
+                            store?.updateToken(res.body.token)
+                            goToFeedScreen()
+                        }
+
+                        is Response.Error.HttpError -> {
+                            Toast.makeText(
+                                context,
+                                "Произошла ошибка: ${res.errorBody?.reason}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                        is Response.Error.NetworkError -> {
+                            Toast.makeText(
+                                context,
+                                "Произошла ошибка: Нет доступа к интернету",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                        is Response.Error.SerializationError -> {
+                            Toast.makeText(
+                                context,
+                                "Произошла ошибка: Ошибка разбора ответа",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                }
             }
-            goToFeedScreen()
         }
     }
 
