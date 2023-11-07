84bfd0f4a6ce92dcffe4e0201bea139b5da3bca8
Daniil <dadyarri@gmail.com>
2023-11-07 14:21:35 +0300

    add fetching data from server

--- app/src/main/java/ru/unilms/domain/quiz/view/screen/QuizInfoScreen.kt
+++ app/src/main/java/ru/unilms/domain/quiz/view/screen/QuizInfoScreen.kt
@@ -9,15 +9,22 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.launch
 import ru.unilms.R
 import ru.unilms.data.AppBarState
 import ru.unilms.domain.app.util.Screens
 import ru.unilms.domain.common.unit.minutes
+import ru.unilms.domain.quiz.model.QuizInfo
 import ru.unilms.domain.quiz.viewmodel.QuizInfoViewModel
 import java.util.UUID
 
@@ -27,12 +34,22 @@ fun QuizInfoScreen(
     onComposing: (AppBarState) -> Unit,
     navigate: (Screens, UUID, Int) -> Unit
 ) {
+    val coroutineScope = rememberCoroutineScope()
     val viewModel = hiltViewModel<QuizInfoViewModel>()
+    var quizInfo: QuizInfo? by remember { mutableStateOf(null) }
+
+    fun updateQuizInfo() = coroutineScope.launch {
+        quizInfo = viewModel.getQuizInfo(quizId)
+    }
 
     LaunchedEffect(true) {
+        updateQuizInfo()
+    }
+
+    LaunchedEffect(quizInfo) {
         onComposing(
             AppBarState(
-                title = viewModel.data.visibleName
+                title = quizInfo?.visibleName
             )
         )
     }
@@ -47,7 +64,7 @@ fun QuizInfoScreen(
             headlineContent = { Text(text = stringResource(id = R.string.label_quiz_amount_of_questions)) },
             trailingContent = {
                 Text(
-                    text = viewModel.data.amountOfQuestions.toString()
+                    text = quizInfo?.amountOfQuestions.toString()
                 )
             }
         )
@@ -55,7 +72,7 @@ fun QuizInfoScreen(
             headlineContent = { Text(text = stringResource(id = R.string.label_quiz_amount_of_attempts)) },
             trailingContent = {
                 Text(
-                    text = viewModel.data.amountOfAttempts.toString()
+                    text = quizInfo?.amountOfAttempts.toString()
                 )
             }
         )
@@ -63,7 +80,7 @@ fun QuizInfoScreen(
             headlineContent = { Text(text = stringResource(id = R.string.label_quiz_try_time_limit)) },
             trailingContent = {
                 Text(
-                    text = viewModel.data.timeLimit.minutes
+                    text = quizInfo?.timeLimit?.minutes ?: ""
                 )
             }
         )
