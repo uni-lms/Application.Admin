2a5013c1702efceecfc887eb5e4d31397f6c48a6
Daniil <dadyarri@gmail.com>
2023-11-13 22:22:59 +0300

    add empty manage screen to admin variant

---
+++ app/src/admin/java/ru/unilms/domain/manage/view/screen/ManageScreen.kt
@@ -0,0 +1,38 @@
+package ru.unilms.domain.manage.view.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ru.unilms.data.AppBarState
+import ru.unilms.data.FabState
+import ru.unilms.domain.app.util.Screens
+import java.util.UUID
+
+@Composable
+fun ManageScreen(navigate: (Screens, UUID?) -> Unit, onComposing: (AppBarState, FabState) -> Unit) {
+    LaunchedEffect(true) {
+        onComposing(
+            AppBarState(
+                actions = {}
+            ),
+            FabState(
+                fab = {}
+            )
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+
+    }
+}
