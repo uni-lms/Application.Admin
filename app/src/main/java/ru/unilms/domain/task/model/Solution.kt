6fddbde2001e755ff2d88d24715b1e175f87aa8b
Daniil <dadyarri@gmail.com>
2023-11-23 15:51:25 +0300

    add displaying solutions on task screen

--- app/src/main/java/ru/unilms/domain/task/model/Solution.kt
+++ app/src/main/java/ru/unilms/domain/task/model/Solution.kt
@@ -3,20 +3,15 @@ package ru.unilms.domain.task.model
 import kotlinx.serialization.Serializable
 import ru.unilms.domain.common.serialization.InstantSerializer
 import ru.unilms.domain.common.serialization.UUIDSerializer
-import ru.unilms.domain.task.util.enums.TaskStatus
 import java.time.Instant
 import java.util.UUID
 
 @Serializable
-data class TaskInfo(
+data class Solution(
     @Serializable(UUIDSerializer::class)
     val id: UUID,
-    val title: String,
-    val description: String?,
     @Serializable(InstantSerializer::class)
-    val availableUntil: Instant,
-    val maximumPoints: Int,
-    val rating: Int,
-    val status: TaskStatus = TaskStatus.NotSent,
-    val solutions: List<Solution>,
+    val dateTime: Instant,
+    val amountOfFiles: Int,
+    val status: String,
 )
--- app/src/main/java/ru/unilms/domain/task/view/screen/TaskScreen.kt
+++ app/src/main/java/ru/unilms/domain/task/view/screen/TaskScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -18,6 +20,7 @@ import androidx.compose.material.icons.outlined.TimerOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -121,6 +124,34 @@ fun TaskScreen(
                 )
             }
         )
+
+        if (taskInfo != null && taskInfo!!.solutions.isNotEmpty()) {
+            Text(
+                text = stringResource(R.string.label_task_solutions),
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            LazyColumn {
+                items(
+                    items = taskInfo!!.solutions,
+                    itemContent = {
+                        ListItem(
+                            headlineContent = {
+                                Text(text = it.dateTime.formatAsString())
+                            },
+                            overlineContent = {
+                                Text(
+                                    text = stringResource(
+                                        R.string.label_task_solution_amount_of_files,
+                                        it.amountOfFiles
+                                    )
+                                )
+                            }
+                        )
+                    }
+                )
+            }
+        }
 
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             Button(onClick = { navigate(Screens.SubmitAnswer, taskId) }) {
