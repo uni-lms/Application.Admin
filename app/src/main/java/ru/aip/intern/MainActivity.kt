4c2541ce5aaf915807333d7d4828593bbaa78c2a
Daniil <dadyarri@gmail.com>
2024-02-29 16:29:36 +0300

    add some simple base screen

--- app/src/main/java/ru/aip/intern/MainActivity.kt
+++ app/src/main/java/ru/aip/intern/MainActivity.kt
@@ -3,41 +3,21 @@ package ru.aip.intern
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import ru.aip.intern.ui.components.BaseScreen
+import ru.aip.intern.ui.components.Greeting
 import ru.aip.intern.ui.theme.AltenarInternshipTheme
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Locale.setDefault(Locale("ru", "RU"))
         setContent {
             AltenarInternshipTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                BaseScreen {
                     Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AltenarInternshipTheme {
-        Greeting("Android")
     }
 }
