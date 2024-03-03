6664addd3b22f1d5d4020181edf425754e913977
Daniil <dadyarri@gmail.com>
2024-03-03 12:08:26 +0300

    add top bar

---
+++ app/src/main/java/ru/aip/intern/ui/components/TopBar.kt
@@ -0,0 +1,44 @@
+package ru.aip.intern.ui.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ru.aip.intern.navigation.Screen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    currentScreen: Screen,
+    canGoBack: Boolean,
+    goUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    title: String? = null,
+    actions: @Composable RowScope.() -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(title ?: currentScreen.title, style = MaterialTheme.typography.titleLarge)
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canGoBack) {
+                IconButton(onClick = { goUp() }) {
+                    Icon(imageVector = Icons.Outlined.ArrowBack, contentDescription = null)
+                }
+            }
+        },
+        actions = actions
+    )
+}
