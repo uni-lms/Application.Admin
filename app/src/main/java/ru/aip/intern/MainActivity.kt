157391c2c2b79c8e6f0494a7e010df33f43e45ae
Daniil <dadyarri@gmail.com>
2024-03-03 16:52:24 +0300

    fix di

--- app/src/main/java/ru/aip/intern/MainActivity.kt
+++ app/src/main/java/ru/aip/intern/MainActivity.kt
@@ -3,10 +3,12 @@ package ru.aip.intern
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import ru.aip.intern.ui.screens.AipApp
 import ru.aip.intern.ui.theme.AltenarInternshipTheme
 import java.util.Locale
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
