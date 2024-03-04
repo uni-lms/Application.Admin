e40d2e95b8cd6eab4860985367794401869069ad
Daniil <dadyarri@gmail.com>
2024-03-04 16:39:17 +0300

    add pull-to-refresh on menu screen and random amount of notifications

--- app/src/main/java/ru/aip/intern/ui/screens/MenuScreen.kt
+++ app/src/main/java/ru/aip/intern/ui/screens/MenuScreen.kt
@@ -1,45 +1,66 @@
 package ru.aip.intern.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Badge
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import ru.aip.intern.navigation.Screen
 import ru.aip.intern.navigation.ScreenPosition
 import ru.aip.intern.ui.components.BaseScreen
+import ru.aip.intern.viewmodels.MenuViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MenuScreen(title: MutableState<String>) {
     // TODO real API call to get notifications count
-    val unreadNotificationsCount by remember { mutableIntStateOf(1) }
     title.value = "Меню"
 
-    BaseScreen {
-        enumValues<Screen>().filter { screen ->
-            screen.icon != null && screen.position == ScreenPosition.Menu
-        }.forEach { screen ->
-            ListItem(
-                headlineContent = { Text(text = screen.title) },
-                leadingContent = {
-                    if (screen.icon != null) {
-                        Icon(screen.icon, null)
-                    }
-                },
-                trailingContent = {
-                    if (screen == Screen.Notifications && unreadNotificationsCount > 0) {
-                        Badge {
-                            Text(text = unreadNotificationsCount.toString())
+    val viewModel: MenuViewModel = hiltViewModel()
+    val refreshing = viewModel.isRefreshing.observeAsState(false)
+    val unreadNotificationsCount = viewModel.unreadNotificationsCount.observeAsState(0)
+
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = refreshing.value,
+        onRefresh = { viewModel.refresh() }
+    )
+
+    Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
+        BaseScreen {
+            enumValues<Screen>().filter { screen ->
+                screen.icon != null && screen.position == ScreenPosition.Menu
+            }.forEach { screen ->
+                ListItem(
+                    headlineContent = { Text(text = screen.title) },
+                    leadingContent = {
+                        if (screen.icon != null) {
+                            Icon(screen.icon, null)
+                        }
+                    },
+                    trailingContent = {
+                        if (screen == Screen.Notifications && unreadNotificationsCount.value > 0) {
+                            Badge {
+                                Text(text = unreadNotificationsCount.value.toString())
+                            }
                         }
                     }
-                }
-            )
+                )
+            }
         }
+        PullRefreshIndicator(
+            refreshing.value,
+            pullRefreshState,
+            Modifier.align(Alignment.TopCenter)
+        )
     }
 }
---
+++ app/src/main/java/ru/aip/intern/viewmodels/MenuViewModel.kt
@@ -0,0 +1,35 @@
+package ru.aip.intern.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import kotlin.random.Random
+
+@HiltViewModel
+class MenuViewModel @Inject constructor() : ViewModel() {
+
+    private val _isRefreshing = MutableLiveData(false)
+    private val _unreadNotificationsCount = MutableLiveData(0)
+
+    val unreadNotificationsCount: LiveData<Int> = _unreadNotificationsCount
+    val isRefreshing: LiveData<Boolean> = _isRefreshing
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        viewModelScope.launch {
+            _isRefreshing.value = true
+            delay(300)
+            _unreadNotificationsCount.value = Random.nextInt(0, 3)
+            _isRefreshing.value = false
+        }
+    }
+
+}
