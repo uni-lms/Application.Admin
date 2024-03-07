916478bf1b9e8323bac093f05b6c470d56ba5ea6
Daniil <dadyarri@gmail.com>
2024-03-07 18:46:41 +0300

    update hilt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
     kotlin("plugin.serialization") version "1.9.22" apply false
 }
