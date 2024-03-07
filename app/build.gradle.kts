82ced9e0acde440c04d3cc7043ce6e28c3ada344
Daniil <dadyarri@gmail.com>
2024-03-07 18:20:47 +0300

    update hilt version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,9 +70,10 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
     implementation("com.google.firebase:firebase-analytics")
 
-    implementation("com.google.dagger:hilt-android:2.49")
+    implementation("com.google.dagger:hilt-android:2.51")
+    kapt("com.google.dagger:hilt-android-compiler:2.51")
+
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
 
     implementation("androidx.compose.runtime:runtime-livedata")
 
