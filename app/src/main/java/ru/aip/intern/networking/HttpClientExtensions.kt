77b0aa2dfd550ef6e217911d1b1c0190eed74e6c
Daniil <dadyarri@gmail.com>
2024-03-07 22:34:05 +0300

    improve response parsing; add not found handler

--- app/src/main/java/ru/aip/intern/networking/HttpClientExtensions.kt
+++ app/src/main/java/ru/aip/intern/networking/HttpClientExtensions.kt
@@ -1,18 +1,19 @@
 package ru.aip.intern.networking
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.plugins.ResponseException
 import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.header
 import io.ktor.client.request.request
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.utils.io.errors.IOException
 import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
 
 suspend inline fun <reified T> HttpClient.safeRequest(block: HttpRequestBuilder.() -> Unit): Response<T> {
     return try {
@@ -24,13 +25,19 @@ suspend inline fun <reified T> HttpClient.safeRequest(block: HttpRequestBuilder.
         when (response.status) {
             HttpStatusCode.OK -> Response(
                 isSuccess = true,
-                value = response.body(),
+                value = response.parseBody<T>()!!.value,
                 errorMessage = null
             )
 
             HttpStatusCode.Unauthorized -> Response(
                 isSuccess = false,
                 errorMessage = response.headers["www-authenticate"] ?: "Не авторизован",
+                value = null
+            )
+
+            HttpStatusCode.NotFound -> Response(
+                isSuccess = false,
+                errorMessage = "Не найдено: ${response.parseBody<T>()!!.errors[0]}",
                 value = null
             )
 
@@ -50,9 +57,11 @@ suspend inline fun <reified T> HttpClient.safeRequest(block: HttpRequestBuilder.
     }
 }
 
-suspend inline fun <reified E> ResponseException.errorBody(): E? {
+suspend inline fun <reified T> HttpResponse.parseBody(): ResponseWrapper<T>? {
+    val json = Json { ignoreUnknownKeys = true }
     return try {
-        response.body()
+        val stringBody = bodyAsText()
+        json.decodeFromString<ResponseWrapper<T>>(stringBody)
     } catch (e: SerializationException) {
         null
     }
