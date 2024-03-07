7335505243d081b820243a4f616e4d291140d0b9
Daniil <dadyarri@gmail.com>
2024-03-07 17:11:42 +0300

    add manager to manipulate permissions

--- app/src/main/java/ru/aip/intern/MainActivity.kt
+++ app/src/main/java/ru/aip/intern/MainActivity.kt
@@ -3,6 +3,8 @@ package ru.aip.intern
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import dagger.hilt.android.AndroidEntryPoint
 import ru.aip.intern.ui.screens.AipApp
 import ru.aip.intern.ui.theme.AltenarInternshipTheme
@@ -10,13 +12,33 @@ import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Locale.setDefault(Locale("ru", "RU"))
+
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue with the operation
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features require a permission that the user has denied.
+            }
+        }
+
         setContent {
             AltenarInternshipTheme {
                 AipApp()
             }
         }
     }
+
+    fun getPermissionLauncher(): ActivityResultLauncher<String> {
+        return requestPermissionLauncher
+    }
+
 }
