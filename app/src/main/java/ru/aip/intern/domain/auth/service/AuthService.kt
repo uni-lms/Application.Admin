91f6fd395fd145acbe9140efe1180d3eec11bb86
Daniil <dadyarri@gmail.com>
2024-03-10 11:09:38 +0300

    add whoami endpoint call

--- app/src/main/java/ru/aip/intern/domain/auth/service/AuthService.kt
+++ app/src/main/java/ru/aip/intern/domain/auth/service/AuthService.kt
@@ -5,10 +5,12 @@ import io.ktor.client.request.headers
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.contentType
 import ru.aip.intern.domain.auth.data.LoginRequest
 import ru.aip.intern.domain.auth.data.LoginResponse
+import ru.aip.intern.domain.auth.data.WhoamiResponse
 import ru.aip.intern.networking.HttpClientFactory
 import ru.aip.intern.networking.Response
 import ru.aip.intern.networking.safeRequest
@@ -24,6 +26,19 @@ class AuthService(private val token: String) {
             accept(ContentType.Application.Json)
             headers {
                 append("x-FCM-Token", fcmToken)
+            }
+        }
+    }
+
+    suspend fun whoami(): Response<WhoamiResponse> {
+        val httpClient = HttpClientFactory.httpClient
+        return httpClient.safeRequest {
+            method = HttpMethod.Get
+            url("/auth/whoami")
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+            headers {
+                append(HttpHeaders.Authorization, token)
             }
         }
     }
