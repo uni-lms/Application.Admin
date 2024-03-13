86f38ae6a1364a32f52b8f8de647960003a40c7d
Daniil <dadyarri@gmail.com>
2024-03-13 13:26:07 +0300

    add injecting viewmodel with parameter

--- app/src/main/java/ru/aip/intern/ui/screens/InternshipScreen.kt
+++ app/src/main/java/ru/aip/intern/ui/screens/InternshipScreen.kt
@@ -2,9 +2,11 @@ package ru.aip.intern.ui.screens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.hilt.navigation.compose.hiltViewModel
 import ru.aip.intern.navigation.Screen
 import ru.aip.intern.ui.components.BaseScreen
 import ru.aip.intern.ui.components.Greeting
+import ru.aip.intern.viewmodels.InternshipViewModel
 import java.util.UUID
 
 @Composable
@@ -15,6 +17,10 @@ fun InternshipScreen(
 ) {
 
     title.value = "Стажировка"
+
+    val viewModel = hiltViewModel<InternshipViewModel, InternshipViewModel.Factory>(
+        creationCallback = { factory -> factory.create(internshipId) }
+    )
 
     BaseScreen {
         Greeting(name = "Стажировка")
