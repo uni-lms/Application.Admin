77204da3433e909950eda9366b75753d647497fb
Daniil <dadyarri@gmail.com>
2024-03-13 13:26:33 +0300

    add method to get internship's content

--- app/src/main/java/ru/aip/intern/domain/internships/service/InternshipsService.kt
+++ app/src/main/java/ru/aip/intern/domain/internships/service/InternshipsService.kt
@@ -7,10 +7,12 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import ru.aip.intern.auth.AuthManager
+import ru.aip.intern.domain.internships.data.Content
 import ru.aip.intern.domain.internships.data.Internship
 import ru.aip.intern.networking.HttpClientFactory
 import ru.aip.intern.networking.Response
 import ru.aip.intern.networking.safeRequest
+import java.util.UUID
 import javax.inject.Inject
 
 class InternshipsService @Inject constructor(private val authManager: AuthManager) {
@@ -33,6 +35,19 @@ class InternshipsService @Inject constructor(private val authManager: AuthManage
         return httpClient.safeRequest {
             method = HttpMethod.Get
             url("/internships/owned")
+            accept(ContentType.Application.Json)
+            headers {
+                append(HttpHeaders.Authorization, authHeaderValue)
+            }
+        }
+    }
+
+    suspend fun getContent(id: UUID): Response<Content> {
+        val httpClient = HttpClientFactory.httpClient
+        val authHeaderValue = authManager.getAuthHeaderValue()
+        return httpClient.safeRequest {
+            method = HttpMethod.Get
+            url("/content/${id}")
             accept(ContentType.Application.Json)
             headers {
                 append(HttpHeaders.Authorization, authHeaderValue)
