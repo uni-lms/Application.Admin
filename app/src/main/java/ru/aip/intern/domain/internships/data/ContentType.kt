3a3365a88e7941d06d3d644808a5fa8d46cdd42e
Daniil <dadyarri@gmail.com>
2024-03-15 10:38:28 +0300

    register serializer for content type enum

--- app/src/main/java/ru/aip/intern/domain/internships/data/ContentType.kt
+++ app/src/main/java/ru/aip/intern/domain/internships/data/ContentType.kt
@@ -1,5 +1,9 @@
 package ru.aip.intern.domain.internships.data
 
+import kotlinx.serialization.Serializable
+import ru.aip.intern.serialization.ContentTypeSerializer
+
+@Serializable(ContentTypeSerializer::class)
 enum class ContentType(val typeName: String) {
     Text("text"),
     Link("link"),
