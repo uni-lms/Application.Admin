83200674ccc1cd2f43cdb8ee2757948a3b1e1671
Daniil <dadyarri@gmail.com>
2024-03-26 18:30:10 +0300

    add normal ui to SplashScreen

--- app/src/main/java/ru/aip/intern/ui/fragments/SplashScreen.kt
+++ app/src/main/java/ru/aip/intern/ui/fragments/SplashScreen.kt
@@ -1,17 +1,27 @@
 package ru.aip.intern.ui.fragments
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import ru.aip.intern.R
 import ru.aip.intern.navigation.Screen
-import ru.aip.intern.ui.components.BaseScreen
 import ru.aip.intern.viewmodels.StartScreenViewModel
 
 @Composable
@@ -24,12 +34,30 @@ fun SplashScreen(onLoadingComplete: (Screen) -> Unit) {
         startScreen?.let(onLoadingComplete)
     }
 
-    BaseScreen {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            // Replace with your app's logo
+            Icon(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "App Logo",
+                modifier = Modifier.size(120.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "AIP",
+                style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.onBackground)
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary,
+                strokeWidth = 4.dp
+            )
         }
     }
 
