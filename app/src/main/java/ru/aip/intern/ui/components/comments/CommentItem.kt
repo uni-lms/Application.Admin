f9aecdb35e81e82fe987e118d0771475b85a43c4
Daniil <dadyarri@gmail.com>
2024-03-31 23:28:48 +0300

    replace modal with button under the comment and relative time in overline

--- app/src/main/java/ru/aip/intern/ui/components/comments/CommentItem.kt
+++ app/src/main/java/ru/aip/intern/ui/components/comments/CommentItem.kt
@@ -1,72 +1,54 @@
 package ru.aip.intern.ui.components.comments
 
-import androidx.compose.foundation.clickable
+import android.text.format.DateUtils
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.Reply
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material.icons.automirrored.outlined.Comment
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import ru.aip.intern.domain.content.assignment.data.Comment
-import ru.aip.intern.util.format
+import java.time.ZoneId
 
 @Composable
 fun CommentItem(comment: Comment, level: Int) {
     val indent = buildString { repeat(level) { append("• ") } }
-    var isModalOpened by remember { mutableStateOf(false) }
 
     Column(modifier = Modifier.padding(PaddingValues(start = (8 * level).dp))) {
         ListItem(
-            modifier = Modifier.clickable {
-                isModalOpened = true
-            },
             leadingContent = {
                 Text(text = indent)
             },
             overlineContent = {
-                Text(text = comment.author)
+                Text(
+                    text = "${comment.author} • ${
+                        DateUtils.getRelativeTimeSpanString(
+                            comment.createdAt.atZone(
+                                ZoneId.of("UTC")
+                            ).toInstant().toEpochMilli()
+                        )
+                    }"
+                )
             },
             headlineContent = {
                 Text(text = comment.text)
-            }
-        )
-    }
-
-
-    if (isModalOpened) {
-        AlertDialog(
-            onDismissRequest = { isModalOpened = false },
-            confirmButton = { },
-            text = {
-                Column {
-                    ListItem(
-                        headlineContent = { Text(text = "Отправлен") },
-                        trailingContent = { Text(text = comment.createdAt.format()) }
-                    )
-                    ListItem(
-                        modifier = Modifier.clickable {
-                            isModalOpened = false
-                        },
-                        leadingContent = {
-                            Icon(
-                                imageVector = Icons.AutoMirrored.Outlined.Reply,
-                                contentDescription = null
-                            )
-                        },
-                        headlineContent = { Text(text = "Ответить") }
+            },
+            supportingContent = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Outlined.Comment,
+                        contentDescription = null,
+                        modifier = Modifier.size(15.dp)
                     )
                 }
-            },
+            }
         )
     }
 
