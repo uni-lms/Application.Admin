14dfdea5fb60a6d17131225494ae2f938dfa8d6b
Daniil <dadyarri@gmail.com>
2024-04-01 20:20:49 +0300

    add toggle visibility button to password on login screen

--- app/src/main/java/ru/aip/intern/ui/screens/LoginScreen.kt
+++ app/src/main/java/ru/aip/intern/ui/screens/LoginScreen.kt
@@ -9,22 +9,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ru.aip.intern.navigation.Screen
@@ -64,6 +74,7 @@ fun LoginScreen(
         val emailErrorMessage = viewModel.emailErrorMessage.observeAsState("")
         val isPasswordWithError = viewModel.isPasswordWithError.observeAsState(false)
         val passwordErrorMessage = viewModel.passwordErrorMessage.observeAsState("")
+        var passwordVisible by rememberSaveable { mutableStateOf(false) }
 
         val refreshing = viewModel.isRefreshing.observeAsState(false)
         val pullRefreshState = rememberPullRefreshState(
@@ -133,6 +144,10 @@ fun LoginScreen(
                         keyboardType = KeyboardType.Password,
                         imeAction = ImeAction.Done
                     ),
+                    visualTransformation = if (passwordVisible)
+                        VisualTransformation.None
+                    else
+                        PasswordVisualTransformation(),
                     keyboardActions = KeyboardActions(
                         onDone = {
                             submit()
@@ -149,6 +164,16 @@ fun LoginScreen(
                         Text(
                             text = "Пароль"
                         )
+                    },
+                    trailingIcon = {
+                        val image = if (passwordVisible)
+                            Icons.Outlined.Visibility
+                        else Icons.Outlined.VisibilityOff
+
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(imageVector = image, null)
+                        }
+
                     }
                 )
 
