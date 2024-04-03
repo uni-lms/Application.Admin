9b7a9c8006486edbb0b2ae861342e0401220ddff
Daniil <dadyarri@gmail.com>
2024-04-04 00:08:36 +0300

    add some models for fetching info required for creating events

---
+++ app/src/main/java/ru/aip/intern/domain/events/data/EventCreatingInfo.kt
@@ -0,0 +1,10 @@
+package ru.aip.intern.domain.events.data
+
+import kotlinx.serialization.Serializable
+import ru.aip.intern.domain.internships.data.Internship
+
+@Serializable
+data class EventCreatingInfo(
+    val internships: List<Internship>,
+    val users: List<User>
+)
