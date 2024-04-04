74376ddd62e56f0d0a3e5b8c152a0672a0e38993
Daniil <dadyarri@gmail.com>
2024-04-04 21:58:43 +0300

    add date picker and two time pickers

--- app/src/main/java/ru/aip/intern/ui/screens/CreateEventScreen.kt
+++ app/src/main/java/ru/aip/intern/ui/screens/CreateEventScreen.kt
@@ -1,26 +1,49 @@
 package ru.aip.intern.ui.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerLayoutType
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ru.aip.intern.ui.components.BaseScreen
+import ru.aip.intern.ui.components.form.DoubleValueDisplay
 import ru.aip.intern.ui.components.form.MultiSelectComboBox
+import ru.aip.intern.ui.components.form.TimePickerDialog
+import ru.aip.intern.ui.components.form.ValueDisplayWithButton
 import ru.aip.intern.ui.components.form.toComboBoxItemList
+import ru.aip.intern.util.epochDateToNullableString
+import ru.aip.intern.util.formatTimeFromPair
+import ru.aip.intern.util.is24HourFormat
 import ru.aip.intern.viewmodels.CreateEventViewModel
 import java.util.UUID
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun CreateEventScreen(title: MutableState<String>) {
     title.value = "Запланировать событие"
@@ -37,8 +60,48 @@ fun CreateEventScreen(title: MutableState<String>) {
         onRefresh = { viewModel.refresh() }
     )
 
+    val datePickerState = rememberDatePickerState()
+    var showDatePicker by rememberSaveable { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val startTimePickerState = rememberTimePickerState(is24Hour = is24HourFormat(context))
+    val endTimePickerState = rememberTimePickerState(is24Hour = is24HourFormat(context))
+
+    var showStartTimePicker by rememberSaveable { mutableStateOf(false) }
+    var showEndTimePicker by rememberSaveable { mutableStateOf(false) }
+
     Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
         BaseScreen {
+
+            ValueDisplayWithButton(
+                title = "Дата события",
+                value = datePickerState.selectedDateMillis.epochDateToNullableString()
+            ) {
+                showDatePicker = true
+            }
+
+            DoubleValueDisplay(
+                title = "Время события",
+                leftValue = Pair(startTimePickerState.hour, startTimePickerState.minute)
+                    .formatTimeFromPair(
+                        context
+                    ),
+                rightValue = Pair(endTimePickerState.hour, endTimePickerState.minute)
+                    .formatTimeFromPair(
+                        context
+                    ),
+                divider = {
+                    HorizontalDivider(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .width(10.dp),
+                        thickness = 1.dp,
+                    )
+                },
+                onLeftClick = { showStartTimePicker = true },
+                onRightClick = { showEndTimePicker = true }
+            )
+
             MultiSelectComboBox(
                 title = "Выберите участвующие стажировки",
                 items = eventCreatingInfo.internships.toComboBoxItemList({ it.id }, { it.name })
@@ -54,6 +117,55 @@ fun CreateEventScreen(title: MutableState<String>) {
                 selectedUsers.clear()
                 selectedUsers.addAll(it)
             }
+
+            if (showDatePicker) {
+                DatePickerDialog(
+                    onDismissRequest = { showDatePicker = false },
+                    confirmButton = {
+                        TextButton(onClick = { showDatePicker = false }) {
+                            Text("Выбрать")
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(onClick = { showDatePicker = false }) {
+                            Text("Отменить")
+                        }
+                    }
+                ) {
+                    DatePicker(state = datePickerState)
+                }
+            }
+
+            if (showStartTimePicker) {
+                TimePickerDialog(
+                    title = "Выберите время",
+                    onDismissRequest = { showStartTimePicker = false },
+                    onConfirm = { showStartTimePicker = false }
+                ) {
+                    TimePicker(
+                        state = startTimePickerState,
+                        layoutType = TimePickerLayoutType.Vertical,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+
+            }
+
+            if (showEndTimePicker) {
+                TimePickerDialog(
+                    title = "Выберите время",
+                    onDismissRequest = { showEndTimePicker = false },
+                    onConfirm = { showEndTimePicker = false }
+                ) {
+                    TimePicker(
+                        state = endTimePickerState,
+                        layoutType = TimePickerLayoutType.Vertical,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+
+            }
+
         }
         PullRefreshIndicator(
             refreshing.value,
