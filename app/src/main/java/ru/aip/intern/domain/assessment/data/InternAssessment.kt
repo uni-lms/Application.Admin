828c1bc03b70f342dff2426244191792b623b539
Daniil <dadyarri@gmail.com>
2024-04-25 14:57:47 +0300

    add id to intern assessment

--- app/src/main/java/ru/aip/intern/domain/assessment/data/InternAssessment.kt
+++ app/src/main/java/ru/aip/intern/domain/assessment/data/InternAssessment.kt
@@ -1,9 +1,13 @@
 package ru.aip.intern.domain.assessment.data
 
 import kotlinx.serialization.Serializable
+import ru.aip.intern.serialization.UuidSerializer
+import java.util.UUID
 
 @Serializable
 data class InternAssessment(
+    @Serializable(UuidSerializer::class)
+    val id: UUID,
     val internName: String,
     val assessments: List<Assessment>
 )
