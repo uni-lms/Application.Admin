4dde1806b5152a85cea7c461fc1420e3f09b856e
Daniil <dadyarri@gmail.com>
2024-04-26 20:11:32 +0300

    fix updating title; add pull refresh indicator

--- app/src/main/java/ru/aip/intern/ui/screens/QuizScreen.kt
+++ app/src/main/java/ru/aip/intern/ui/screens/QuizScreen.kt
@@ -2,13 +2,17 @@ package ru.aip.intern.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import ru.aip.intern.R
@@ -26,8 +30,6 @@ fun QuizScreen(
     navigate: (Screen, UUID) -> Unit
 ) {
 
-    title.value = stringResource(R.string.quiz)
-
     val viewModel: QuizViewModel =
         hiltViewModel<QuizViewModel, QuizViewModel.Factory>(
             creationCallback = { factory -> factory.create(quizId) }
@@ -39,11 +41,30 @@ fun QuizScreen(
         refreshing = isRefreshing,
         onRefresh = { viewModel.refresh(quizId) }
     )
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        if (quizData.title.isEmpty()) {
+            title.value = context.getString(R.string.quiz)
+        }
+    }
+
+    LaunchedEffect(quizData.title) {
+        if (quizData.title.isNotEmpty()) {
+            title.value = quizData.title
+        }
+    }
 
     Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
         BaseScreen {
             Greeting(name = stringResource(R.string.quiz))
         }
+
+        PullRefreshIndicator(
+            isRefreshing,
+            pullRefreshState,
+            Modifier.align(Alignment.TopCenter)
+        )
     }
 
 }
