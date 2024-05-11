211f58233fdeacd8d308aa4a701564db220952ae
Daniil <dadyarri@gmail.com>
2024-05-11 18:15:45 +0300

    add attempt id

--- app/src/main/java/ru/aip/intern/domain/content/quiz/data/QuizPassAttempt.kt
+++ app/src/main/java/ru/aip/intern/domain/content/quiz/data/QuizPassAttempt.kt
@@ -2,10 +2,14 @@ package ru.aip.intern.domain.content.quiz.data
 
 import kotlinx.serialization.Serializable
 import ru.aip.intern.serialization.LocalDateTimeSerializer
+import ru.aip.intern.serialization.UuidSerializer
 import java.time.LocalDateTime
+import java.util.UUID
 
 @Serializable
 data class QuizPassAttempt(
+    @Serializable(UuidSerializer::class)
+    val id: UUID,
     @Serializable(LocalDateTimeSerializer::class)
     val startedAt: LocalDateTime,
     @Serializable(LocalDateTimeSerializer::class)
