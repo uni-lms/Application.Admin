04145695952da85998eebb81c70609226ba82190
Daniil <dadyarri@gmail.com>
2024-05-12 22:30:10 +0300

    add optional customization to base screen

--- app/src/main/java/ru/aip/intern/ui/components/BaseScreen.kt
+++ app/src/main/java/ru/aip/intern/ui/components/BaseScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -19,6 +20,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.unit.dp
@@ -27,7 +29,12 @@ import ru.aip.intern.networking.ConnectivityObserver
 import ru.aip.intern.viewmodels.TopBarViewModel
 
 @Composable
-fun BaseScreen(children: @Composable () -> Unit) {
+fun BaseScreen(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    children: @Composable () -> Unit
+) {
 
     Column(
         Modifier
@@ -65,9 +72,11 @@ fun BaseScreen(children: @Composable () -> Unit) {
                 .padding(28.dp)
         ) {
             Column(
-                modifier = Modifier
+                modifier = modifier
                     .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = horizontalAlignment,
+                verticalArrangement = verticalArrangement
             ) {
                 children()
             }
